5a7a02138d40e1d8096c3530c3bd8e428638655e
Марк <fenster.mark@yandex.ru>
2023-04-08 00:37:52 +0300

    dirty_main.py added

---
+++ dirty_main.py
@@ -0,0 +1,7 @@
+from application.salary import *
+from application.db.people import *
+from application.current_date import *
+
+if __name__ == '__main__':
+    print(f'{cs()}\t{day}.{month}.{year}')
+    print(f'{ge()}\t{day}.{month}.{year}')
