0470042e4883066f38181ba793ee23b6e7c9b92e
Марк <fenster.mark@yandex.ru>
2023-04-08 00:57:06 +0300

    repair dirt_main.py

--- dirty_main.py
+++ dirty_main.py
@@ -2,6 +2,8 @@ from application.salary import *
 from application.db.people import *
 from application.current_date import *
 
+
+
 if __name__ == '__main__':
-    print(f'{cs()}\t{day}.{month}.{year}')
-    print(f'{ge()}\t{day}.{month}.{year}')
+    print(f'{calculate_salary()}\t{day}.{month}.{year}')
+    print(f'{get_employees()}\t{day}.{month}.{year}')
